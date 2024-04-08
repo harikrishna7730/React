f971a7807ae0a097e2f23823b7d11c8b546612d9
harikrishna7730 <abhitangala@gmail.com>
2024-04-08 19:05:52 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import DummyJson from './Filtering/dummyjson';
 
 function App() {
--- src/Filtering/dummyjson.js
+++ src/Filtering/dummyjson.js
@@ -1,38 +1,66 @@
 import { Component } from "react";
 import axios from "axios"
+import "./dummy.css"
 
 class DummyJson extends Component{
 
     state={
         products:[]
     }
-
     componentDidMount(){
         this.FetchData()
     }
-
+//fetching data using axios
 FetchData=async()=>{
     let result=await axios.get("https://dummyjson.com/products")
     console.log(result)
+
+    this.setState({
+        products:result.data.products
+    })
+}
+
+//low to high using sort
+Lowtohigh=()=>{
+    let sorting=this.state.products.sort((a,b)=>a.price-b.price)
+    this.setState({
+        products:sorting
+    })
+}
+
+//High to low
+Hightolow=()=>{
+    let sorting=this.state.products.sort((a,b)=>b.price-a.price)
+    this.setState({
+        products:sorting
+    })
 }
 
     render(){
         
         return(
             <>
-            <h3>Product Listing</h3>
+            <h2 style={{textAlign:"center"}}>Products</h2>
+            <button onClick={this.Lowtohigh}>Low To High</button>
+            <button onClick={this.Hightolow}>High To Low</button>
+        
+            <div className="products">
             {
                 this.state.products.map((eachObj)=>{
-                   
+                   const{title,thumbnail,price}=eachObj
                     return(
-                        <>
-                        <h3>{eachObj.title}</h3>
-                        </>
+                        <div className="cards">
+                        <h3>{title}</h3>
+                        <img src={thumbnail} alt={title}/>
+                        <h3>{price}</h3>
+                        </div>
                     )
                 })
             }
+            </div>
             </>
         )
+      
     }
 }
 export default DummyJson
