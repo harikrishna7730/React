191ef2d75cd404a434b2aa663e15a4305fed6cc3
harikrishna7730 <abhitangala@gmail.com>
2024-04-18 11:41:16 +0530

    add

--- src/navigation-screen/nav-screen.js
+++ src/navigation-screen/nav-screen.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-
-
+import Navbar from "../components/navbar"
 
 const NavigationScreen=()=>{
     return (
         <>
         <BrowserRouter>
         <Routes>
-            <Route path="/Register"   />
+            <Route path="/" Component={Navbar}   />
             <Route/>
             <Route/>
             <Route/>
