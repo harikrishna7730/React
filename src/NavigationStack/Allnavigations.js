8652d0965e84aee613173ae07c6456483d8a3a72
harikrishna7730 <abhitangala@gmail.com>
2024-04-19 16:07:05 +0530

    add

--- src/NavigationStack/Allnavigations.js
+++ src/NavigationStack/Allnavigations.js
@@ -3,12 +3,13 @@ import HomeScreen from "../pages/homeScreen"
 import ContactScreen from "../pages/contactScreen"
 import AboutScreen from "../pages/aboutscreen"
 import { createContext, useState } from "react"
+import Loginpage from "../pages/Login"
 
 
 export const changeings=createContext()
 
 const Navigations=()=>{
-
+const[login,setlogin]=useState(false)
   //for changing the color
   const[color,setcolor]=useState("black")
 
@@ -39,14 +40,26 @@ const Navigations=()=>{
 
       }} >
 
-
-        <BrowserRouter>
+       
+        {
+          login
+          ?
+          <BrowserRouter>
         <Routes>
             <Route path="/" Component={HomeScreen}/>
             <Route path="/contact" Component={ContactScreen} />
             <Route path="/About"  Component={AboutScreen} />
         </Routes>
         </BrowserRouter>
+        :
+        // <BrowserRouter>
+        //    <Routes>
+        //     <Route  path="/Login" Component={Loginpage}/>
+        //    </Routes>
+        // </BrowserRouter>
+        // <h1> helloworkd</h1>
+        <Loginpage/>
+        }
 
 
         </changeings.Provider>
